b7cb6466ab85bba1ba1767a895cd06adaee37a5d
monkeywithoutthee <54135971+monkeywithoutthee@users.noreply.github.com>
2021-03-10 11:30:46 +0000

    messing with style

--- myCharts2-1.js
+++ myCharts2-1.js
@@ -9,43 +9,44 @@ window.addEventListener("resize", function () {
  };
 });
 
-window.addEventListener("mouseover", function (event){
+window.addEventListener("mouseover", function(event){
   //console.log('mouse over resize:', event);
 });
 window.addEventListener('click',function(event){
 //  console.log('clickevent;;',event);
-})
-/*(function loadDetect(){
+});
+(function loadDetect(){
 	function detectDevTool(allow) {
   //  console.log('allow::', allow);
   	if(isNaN(+allow)) allow = 100;
     var start = +new Date();
     debugger;
     var end = +new Date();
-  //  console.log(start,end,end - start,'<< debugger opened!!',allow);
-    if(isNaN(start) || isNaN(end) || end - start > allow) {//<<this is the genius
+    console.log(start,end,end - start,'<< debugger opened!!',allow);
+    if(isNaN(start)||isNaN(end)||end-start>allow){
     	alert('DEVTOOLS detected. all operations will be terminated. Please disable developer tools to view this graph');
-      window.location.href = 'YoYDeathCount.html';
+      return false;
     }
   }
-  if(window.attachEvent) {
-  	if (document.readyState === "complete" || document.readyState === "interactive") {
-    	detectDevTool();
-      window.attachEvent('onresize', detectDevTool);
-      window.attachEvent('onmousemove', detectDevTool);
-      window.attachEvent('onfocus', detectDevTool);
-      window.attachEvent('onblur', detectDevTool);
+    if(window.attachEvent) {
+      console.log('window.attachEvent::',window.attachEvent);
+    	if (document.readyState === "complete" || document.readyState === "interactive") {
+      	detectDevTool();
+        window.attachEvent('onresize', detectDevTool);
+        window.attachEvent('onmousemove', detectDevTool);
+        window.attachEvent('onfocus', detectDevTool);
+        window.attachEvent('onblur', detectDevTool);
+      } else {
+      	setTimeout(argument.callee, 0);
+      }
     } else {
-    	setTimeout(argument.callee, 0);
+    	window.addEventListener('load', detectDevTool);
+      window.addEventListener('resize', detectDevTool);
+      window.addEventListener('mousemove', detectDevTool);
+      window.addEventListener('focus', detectDevTool);
+      window.addEventListener('blur', detectDevTool);
     }
-  } else {
-  	window.addEventListener('load', detectDevTool);
-    window.addEventListener('resize', detectDevTool);
-    window.addEventListener('mousemove', detectDevTool);
-    window.addEventListener('focus', detectDevTool);
-    window.addEventListener('blur', detectDevTool);
-  }
-})();*/
+})();
 var aDAT = [];
 window.onload = init();
 function init(){
